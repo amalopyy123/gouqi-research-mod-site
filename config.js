e7e17004c480c8c23c26a8b80dcfe6d89b92507a
gouqi <amalopyy123@gmail.com>
2026-08-26 23:20:33 +0900

    chore: update Baidu Wangpan sharing link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Update these URLs when publishing a new download or repository location.
 window.SITE_CONFIG = {
-  baiduUrl: 'https://pan.baidu.com/s/1IccQgKFSPqVcHgb02kMOow',
-  baiduCode: '7h77',
+  baiduUrl: 'https://pan.baidu.com/s/1q9-OFrByTK5EYv7J22SbSQ',
+  baiduCode: 'cz94',
   archivePassword: '123',
   githubUrl: 'https://github.com/amalopyy123/gouqi-mgqp-research-mod'
 };
