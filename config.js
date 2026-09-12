33583261b96c79d053828f4769d69e4c788802c4
gouqi <amalopyy123@gmail.com>
2026-09-12 18:55:43 +0900

    feat: update download link and display mod version

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Update these URLs when publishing a new download or repository location.
 window.SITE_CONFIG = {
-  modVersion: 'v1.2.3',
-  baiduUrl: 'https://pan.baidu.com/s/1-OdoFvTrObLh7dsTvuzXtg',
-  baiduCode: 'pk1n',
+  modVersion: 'v1.2.4',
+  baiduUrl: 'https://pan.baidu.com/s/1RW6PWzLI73RArHnSfQZbFQ',
+  baiduCode: 'ty5v',
   archivePassword: '123',
   githubUrl: 'https://github.com/amalopyy123/gouqi-mgqp-research-mod'
 };
